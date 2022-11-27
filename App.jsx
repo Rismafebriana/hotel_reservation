448acaa4ca6744682582992b498ada7dc922cf4b
Rismafebriana <111462571+Rismafebriana@users.noreply.github.com>
2022-11-27 14:04:19 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import Router from "./src/router";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+import { store } from "./src/redux/store";
+import { Provider } from "react-redux";
+import { color } from "./src/utils";
+
+let customFonts = {
+  "Outfit-Regular": require("./src/assets/fonts/Outfit-Regular.ttf"),
+  "Outfit-Bold": require("./src/assets/fonts/Outfit-Bold.ttf"),
+};
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: color.blackPrimary,
+  },
+};
+
+export default function App() {
+  const [loaded] = useFonts(customFonts);
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer theme={MyTheme}>
+        <Router />
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </Provider>
+  );
+}
